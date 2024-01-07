5e5b2a095b4d3d42798f1f74d6163fc31241e454
k0tyak1t <konstantin.shiyanenko@gmail.com>
2024-01-08 00:38:25 +0300

    Visual solution for task 4 was implemented. This code became looking ugly - need to refactor after exam.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import scipy
+import numpy as np
 
 
-def main():
+def task3():
     alpha_values = [.05, .02, .01]
     df_values = [9, 11, 10, 12]
 
     for df in df_values:
         for alpha in alpha_values:
             print_chi2(alpha, df)
+
+def task4_visual():
+    alpha_values = np.linspace(0, .005, 20)
+    df = 20
+    tn = 3.049
+    for alpha in alpha_values:
+        t = get_t_critical(alpha, df)
+        print(f'FOR {alpha: .4f}:   H0 {"is accepted" if h0_is_accepted(tn, t) else "is declined"}   t:   {t: .4f}')
+
+
+def test():
+    print(get_t_critical(.05, 20))
 
 
 def get_chi_minus(alpha: float, df: int) -> float:
@@ -19,7 +32,7 @@ def get_chi_plus(alpha: float, df: int) -> float:
 
 
 def print_chi2(alpha: float, df: int) -> None:
-        print(f'FOR α = {alpha} (γ = {1-alpha} and df = {df})')
+    print(f'FOR α = {alpha} (γ = {1-alpha} and df = {df})')
     print(f'χ+:  {get_chi_plus(alpha, df): .3f}')
     print(f'χ-:  {get_chi_minus(alpha, df): .3f}')
 
@@ -30,5 +43,14 @@ def get_param_from_keyboard() -> dict:
     return {'alpha': alpha, 'df': df}
 
 
+def get_t_critical(alpha, df):
+    return scipy.stats.t.ppf(q=1-alpha, df=df)
+
+
+def h0_is_accepted(tn, t):
+    return tn < t
+
+
 if __name__ == '__main__':
-    main()
+    task4()
+    #test()
